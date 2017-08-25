017b4830745b3d407fbf6604252346dc89aa81e4
Mayrav Herman <mayravherman@gmail.com>
2017-08-25 14:16:48 -0700

    Manually create and print game boards

---
+++ src/minesweeper.js
@@ -0,0 +1,16 @@
+// Hardcoded rows
+const blankLine = '  |   |  '; // Empty row
+const guessLine = '1 |   |  '; // First square selected
+const bombLine = '  | B |  '; // Bomb in center square
+
+// Empty game board
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// Simulated game board
+console.log('This is what a board with a guess and a bomb on it would look like');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
